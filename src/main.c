66d8bc51dfa41fbbea211a224b479023f10bdff9
bgaro <b.garo44@gmail.com>
2022-01-17 21:35:03 -0500

    clean test

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 #include "csv_to_array.h"
 #include "neural_network.h"
 
-#define NUM_THREADS 16
+#define NUM_THREADS 3
 #define INPUT_NEURON 784
 #define HIDDEN_NEURON_1 21
 #define HIDDEN_NEURON 45
@@ -19,10 +19,10 @@
 #define LAYER_NUM 4
 #define TRAINING_SET_SIZE 60000
 #define TEST_SET_SIZE 10000
-#define EPOCH 1866
+#define EPOCH 10 // 1866
 int main()
 {
-srand(0);
+    srand(0);
     FILE *train_vectors_stream = fopen("./data/fashion_mnist_train_vectors.csv", "r");
     if (train_vectors_stream == NULL)
     {
@@ -69,10 +69,6 @@ srand(0);
     int train_prediction_fd = 0;
     int test_prediction_fd = 0;
     int prediction = 0;
-    //int prediction_2 = 0;
-   // float max = 0.0;
-    //int epoch_max = 0;
-    //int cpt = 0;
     char prediction_buffer[3];
     train_prediction_fd = open("trainPredictions", O_CREAT | O_WRONLY, 0644);
     test_prediction_fd = open("actualTestPredictions", O_CREAT | O_WRONLY, 0644);
@@ -259,73 +255,6 @@ srand(0);
             matrix_free(returns[training].error_weight_gradient_bias_hidden_1);
             matrix_free(returns[training].error_weight_gradient_bias_output);
         }
-
-        /*for (training = 0; training < TRAINING_SET_SIZE; training++)
-        {
-            // Feed forward process
-
-            // initialisation of input layer
-
-            matrix_initialize(input_layer_transpose, 1, INPUT_NEURON, input_array[training]);
-            matrix_transpose(input_layer_transpose, input_layer);
-
-            // feed forward on hidden layer 1
-            feed_forward(weight_input_hidden, input_layer, bias_hidden_1, hidden_layer_1, activation_hidden_1_matrix, RELU);
-
-            // feed forward on hidden layer
-            feed_forward(weight_hidden_hidden, activation_hidden_1_matrix, bias_hidden, hidden_layer, activation_hidden_matrix, RELU);
-
-            // feed forward on output layer
-            feed_forward(weight_hidden_output, activation_hidden_matrix, bias_output, output_layer, activation_output_matrix, SOFTMAX);
-
-            // error function gradiant
-
-            // yj - dkj for j in Y (output layer)
-            // matrix_initialize(expected_output_step, expected_output_step->rows, expected_output_step->cols, &expected_output->data[i][0]);
-
-            matrix_initialize(expected_output, expected_output->rows, expected_output->cols, expected_output_array[training]);
-            matrix_transpose(expected_output, derivate_error_output_layer);
-            matrix_subtract(derivate_error_output_layer, activation_output_matrix);
-            matrix_multiply_constant(derivate_error_output_layer, -(1.0 / TRAINING_SET_SIZE));
-            softmax_derivate(activation_output_matrix, derivate_output);
-
-            // dEk/dyj for j in Y (output layer)
-
-            // dEk/dyj for j in Z \ (Y U X) (hidden layer)
-            backward_propagation_neurons(derivate_error_output_layer, derivate_output, derivate_output_activiation, derivate_output_activiation_transpose, weight_hidden_output, derivate_error_hidden_layer_transpose, derivate_error_hidden_layer, SOFTMAX);
-
-            // dEk/dwij for j in Z \ (Y U X) (hidden layer) 3
-            reLU_derivate(hidden_layer, derivate_hidden);
-            backward_propagation_neurons(derivate_error_hidden_layer, derivate_hidden, derivate_hidden_activation, derivate_hidden_activation_transpose, weight_hidden_hidden, derivate_error_hidden_layer_1_transpose, derivate_error_hidden_layer_1, RELU);
-
-            // dEk/dwij for j in Y(output layer)
-
-            backward_propagation_weights(derivate_error_output_layer, derivate_output, derivate_error_activation_output, activation_hidden_matrix, activation_hidden_matrix_transpose, error_weight_gradient_output_step, SOFTMAX);
-
-            // dEk/dwij for j in Z \ (Y U X) (hidden layer) 4
-
-            backward_propagation_weights(derivate_error_hidden_layer, derivate_hidden, derivate_hidden_error, activation_hidden_1_matrix, activation_hidden_1_matrix_transpose, error_weight_gradient_hidden_step, RELU);
-
-            // dEk/dwij for j in Z \ (Y U X) (hidden layer) 3
-
-            reLU_derivate(hidden_layer_1, derivate_hidden_1_activation);
-            backward_propagation_weights(derivate_error_hidden_layer_1, derivate_hidden_1_activation, derivate_hidden_1_error, input_layer, input_layer_transpose, error_weight_gradient_hidden_1_step, RELU);
-
-            matrix_add(error_weight_gradient_output, error_weight_gradient_output_step); // sum of each training set
-            matrix_add(error_weight_gradient_hidden, error_weight_gradient_hidden_step);
-            matrix_add(error_weight_gradient_hidden_1, error_weight_gradient_hidden_1_step);
-
-            // bias of hidden layer 3 update
-            matrix_add(error_weight_gradient_bias_hidden_1, derivate_hidden_1_error);
-
-            // bias of hidden layer 4 update
-            matrix_add(error_weight_gradient_bias_hidden, derivate_hidden_error);
-            // bias of output layer update
-
-            matrix_add(error_weight_gradient_bias_output, derivate_error_activation_output);
-
-            // free memory
-        }*/
         if (epoch % 50 == 0)
         {
             printf("Epoch %d\n", epoch);
@@ -417,44 +346,7 @@ srand(0);
             args[i]->bias_hidden = matrix_copy(bias_hidden);
             args[i]->bias_hidden_1 = matrix_copy(bias_hidden_1);
         }
-
-/*        for (training = 0; training < TEST_SET_SIZE; training++)
-        {
-
-            // Feed forward process
-
-            // initialisation of input layer
-
-            matrix_initialize(input_layer_transpose, 1, INPUT_NEURON, input_array_test[training]);
-            matrix_transpose(input_layer_transpose, input_layer);
-
-            // feed forward on hidden layer 1
-            feed_forward(weight_input_hidden, input_layer, bias_hidden_1, hidden_layer_1, activation_hidden_1_matrix, RELU);
-
-            // feed forward on hidden layer
-            feed_forward(weight_hidden_hidden, activation_hidden_1_matrix, bias_hidden, hidden_layer, activation_hidden_matrix, RELU);
-
-            // feed forward on output layer
-            feed_forward(weight_hidden_output, activation_hidden_matrix, bias_output, output_layer, activation_output_matrix, SOFTMAX);
-            // error function gradiant
-            matrix_initialize(expected_output, expected_output->rows, expected_output->cols, expected_output_array_test[training]);
-            prediction = get_label(activation_output_matrix);
-            prediction_2 = get_label(expected_output);
-            if (prediction == prediction_2)
-                cpt++;
-        }
-
-        printf("EPOCH : %d | ACCURACY : %f \n", epoch, (float)cpt / (float)TEST_SET_SIZE * 100);
-        if (((float)cpt / (float)TEST_SET_SIZE * 100) > max)
-        {
-            max = (float)cpt / (float)TEST_SET_SIZE * 100;
-            epoch_max = epoch;
-        }
-        cpt = 0;
-*/
-        // reset file pointer
     }
-    //printf("best : EPOCH : %d | ACCURACY : %f \n", epoch_max, max);
     for (training = 0; training < TRAINING_SET_SIZE; training++)
     {
 
